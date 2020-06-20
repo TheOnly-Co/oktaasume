99f8649e3440d0d5af3f88a3a5b5c57b30345115
mit <mit945.mc@gmail.com>
2020-06-19 21:49:06 -0700

    add oktaverify

---
+++ oktaPushVerify.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hunkeelin/oktalib"
+	"github.com/hunkeelin/userprompt"
+	"net/http/cookiejar"
+	"os"
+)
+
+func main() {
+	o, err := oktalib.New(&oktalib.NewInput{
+		Org:                 "dev-815627",
+		IdentityProviderArn: "arn:aws:iam::216228501626:saml-provider/Okta_2",
+		SamlURI:             "/app/amazon_aws/exkawa67iQIlhKIxE4x6",
+	})
+	if err != nil {
+		panic(err)
+	}
+	o.Username, o.Password, o.CookieJar = getCredentials()
+	err = o.LdapLogin()
+	if err != nil {
+		panic(err)
+	}
+
+	if len(o.UserAuth.Embedded.Factors) == 0 || len(o.UserAuth.Embedded.Factors) < 1 {
+		panic(fmt.Errorf("Extra verification must be enabled in Okta. Visit https://varomoney.okta.com/enduser/settings."))
+	}
+
+	switch {
+	case searchAuthMethod(o.UserAuth.Embedded.Factors, oktalib.YubiKey):
+		fmt.Println("Congrats on your shiny new Yubikey")
+		code, err := userprompt.UserPrompt("Please give it a squeeze", false)
+		if err != nil {
+			panic(err)
+		}
+		err = o.OktaMfa(oktalib.YubiKey, code)
+		if err != nil {
+			panic(err)
+		}
+	case searchAuthMethod(o.UserAuth.Embedded.Factors, oktalib.MfaPush):
+		err = o.OktaMfa(oktalib.MfaPush, "")
+		if err != nil {
+			panic(err)
+		}
+	case searchAuthMethod(o.UserAuth.Embedded.Factors, oktalib.MfaCode):
+		passcode, err := userprompt.UserPrompt("Enter a token from your mobile authenticator app", false)
+		if err != nil {
+			panic(err)
+		}
+		err = o.OktaMfa(oktalib.MfaCode, passcode)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		panic(fmt.Errorf("No recognized mfa method found please contact your administrator."))
+	}
+	out, err := o.GetAwsCredentials(oktalib.GetAwsCredentialsInput{
+		RoleArn:    "arn:aws:iam::216228501626:role/devops-admin-role",
+		Expiration: 28800,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println("aws_access_key_id", out.AwsAccessKeyId)
+	//fmt.Println("aws_secret_access_key", out.AwsSecretAccessKey)
+	//fmt.Println("aws_session_token", out.AwsSessionToken)
+	id := "aws_access_key_id = " + out.AwsAccessKeyId
+	key := "aws_secret_access_key = " + out.AwsSecretAccessKey
+	token := "aws_session_token = " + out.AwsSessionToken
+    f, err := os.OpenFile("/Users/mitchellchang/.aws/credentials", os.O_RDWR, 0644)
+    if err != nil{
+        panic(err)
+    }
+    _, err = f.WriteString("[default] \n")
+    if err != nil {
+        panic(err)
+    }
+    _, err = f.WriteString(id+ "\n")
+    if err != nil {
+        panic(err)
+    }
+    _, err = f.WriteString(key+ "\n")
+    if err != nil {
+        panic(err)
+    }
+    _, err = f.WriteString(token)
+    if err != nil {
+        panic(err)
+    }
+    err = f.Sync()
+    if err != nil {
+        panic(err)
+    }
+	return
+}
+func searchAuthMethod(sep []oktalib.OktaUserAuthnFactor, s string) bool {
+	for _, i := range sep {
+		if i.FactorType == s {
+			return true
+		}
+	}
+	return false
+}
+
+func getCredentials() (string, string, *cookiejar.Jar) {
+	currentUser := os.Getenv("USER")
+	userName, err := userprompt.UserPromptWithDefault("Enter Okta Username ("+currentUser+")", currentUser, false)
+	if err != nil {
+		panic(err)
+	}
+	pass, err := userprompt.UserPrompt("Enter Okta Password", true)
+	if err != nil {
+		panic(err)
+	}
+	cJar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+	return userName, pass, cJar
+}
