9510f6a4e976a80237892d07cf8a1b5c4448ac2a
mit <mit945.mc@gmail.com>
2020-06-20 10:06:33 -0700

    add files

---
+++ const.go
@@ -0,0 +1,10 @@
+package oktalib
+
+const (
+	// YubiKey the hardware mfa constant
+	YubiKey = "token:hardware"
+	// MfaPush the push notification constant
+	MfaPush = "push"
+	// MfaCode the code input constant
+	MfaCode = "token:software:totp"
+)
