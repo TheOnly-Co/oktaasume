49218a269e6d567eeefb82bbe221f5012abdeedb
mit <mit945.mc@gmail.com>
2020-06-28 18:00:41 -0700

    go formatted

--- writecreds_test.go
+++ writecreds_test.go
@@ -1,24 +1,24 @@
-package main 
+package main
 
 import (
-    "io/ioutil"    
-    "testing"
+	"io/ioutil"
+	"testing"
 )
 
-func TestWriteCreds (t *testing.T) {
-    mockWrite := []byte("hello")
-    err := writeCredToFile(&writeCredToFileInput{
-        location: "/tmp/hello",
-        towrite: mockWrite,
-    })
-    if err != nil {
-        t.Error(err.Error())
-    }
-    f, err := ioutil.ReadFile("/tmp/hello")
-    if err != nil {
-        t.Error(err.Error())
-    }
-    if string(f) != string(mockWrite) {
-        t.Errorf("The input is different than the output")
-    }
+func TestWriteCreds(t *testing.T) {
+	mockWrite := []byte("hello")
+	err := writeCredToFile(&writeCredToFileInput{
+		location: "/tmp/hello",
+		towrite:  mockWrite,
+	})
+	if err != nil {
+		t.Error(err.Error())
+	}
+	f, err := ioutil.ReadFile("/tmp/hello")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if string(f) != string(mockWrite) {
+		t.Errorf("The input is different than the output")
+	}
 }
